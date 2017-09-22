39c3ef25ece7c659c4bd27c44fda1cd3c967e6e1
zhboner <zhboner@gmail.com>
2017-09-22 16:04:21 +1000

    Add rename

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <meta name="theme-color" content="#000000">
     <link rel="manifest" href="/manifest.json">
     <link rel="shortcut icon" href="/favicon.ico">
-<!--    <link href="/build/static/css/main.3e7bb5c2.css" rel="stylesheet">-->
 
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <?php wp_head(); ?>
@@ -14,7 +13,6 @@
 <body>
 <noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
-<!--<script type="text/javascript" src="/build/static/js/main.8a3e42a4.js"></script>-->
 
 <?php wp_footer(); ?>
 </body>
