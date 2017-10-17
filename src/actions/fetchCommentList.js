8b70ee745a2557f1627dfbb935db724a0e2d8681
zhboner <zhboner@gmail.com>
2017-10-17 16:56:24 +1100

    Try to make thread comments
    It is not finished yet.

--- src/actions/fetchCommentList.js
+++ src/actions/fetchCommentList.js
@@ -28,6 +28,9 @@ export const fetchCommentList = (postID, page = 1) => {
         dispatch(requestCommentsList(postID));
         axios.get(url)
             .then((response) => {
+
+            })
+            .then((response) => {
                 const commentList = response.data;
                 let commentObj = changeListToObj(commentList);
 
@@ -67,4 +70,68 @@ const changeListToObj = (commentList) => {
         commentObj[comment.id] = comment;
     });
     return commentObj;
+};
+
+// Reorder the comment list by thread
+const reorderComment = (comments) => {
+    class Node {
+        constructor(value, parent = 0) {
+            this.prev = parent;
+            this.value = value;
+            this.children = [];
+        }
+    }
+
+    const buildCommentTree = () => {
+        let dict = {};      // Map id to node
+        let root = [];      // The array of root id
+        let commentDict = {};
+
+        for (let i = 0; i < comments.length; i++) {
+            const id = parseInt(comments[i].comment_ID);
+            const parentID = parseInt(comments[i].comment_parent);
+
+            commentDict[id] = comments[i];
+
+            // Get the node and set its parent
+            let node = null;
+            if (dict[id]) {                         // If this node can be found in the dict, pick out and set its parent
+                node = dict[id];
+                node.prev = parentID;
+            } else {                                // Else, create a new one
+                node = new Node(id, parentID);
+                dict[id] = node;
+            }
+
+            // Set parent node's children
+            if (parentID !== 0) {
+                let parentNode = null;
+                if (dict[parentID]) {
+                    parentNode = dict[parentID];
+                } else {
+                    parentNode = new Node(id);
+                    dict[parentID] = parentNode;
+                }
+                parentNode.children.push(id)
+            } else {
+                root.push(id);
+            }
+        }
+        return {dict, root, commentDict};
+    };
+
+    // TODO: Not finished
+    const generateCommentsList = (dict, root, commentDict) => {
+        let out = [];
+
+        for (let i = 0; i < root.length; i++) {
+            out.push(commentDict[root[i]]);
+            const children = dict[root[i]].children;
+            if (children.length !== 0) {
+                for (let k = 0; k < children.length; k++) {
+                    out.concat(generateCommentsList(dict, children))
+                }
+            }
+        }
+    }
 };
