4938eb0bb575d6bda9450d45f1093c0775a5730e
zhboner <zhboner@gmail.com>
2017-10-20 15:47:44 +1100

    Optimize the code

--- lib/Custome-endpoints.php
+++ lib/Custome-endpoints.php
@@ -22,11 +22,6 @@ class Comments_By_Post_Route extends WP_REST_Controller {
                 )
             )
         ));
-
-        register_rest_route($namespace, '/nonce', array(
-            'methods'       => WP_REST_Server::READABLE,
-            'callback'      => array($this, 'get_item')
-        ));
     }
 
     public function get_items ($request) {
@@ -36,13 +31,30 @@ class Comments_By_Post_Route extends WP_REST_Controller {
             'post_id' => $post_id
         ));
     }
+}
+
+class Get_Nonce_By_Route extends WP_REST_Controller {
+    public function register_routes() {
+        $version = '1';
+        $namespace = 'zhboner/v' . $version;
+
+        register_rest_route($namespace, '/nonce', array(
+            'methods'       => WP_REST_Server::READABLE,
+            'callback'      => array($this, 'get_item')
+        ));
+    }
 
     public function get_item($request) {
         return wp_create_nonce('wp_rest');
     }
 }
 
+
+
+
 add_action('rest_api_init', function() {
-    $Custome_Route = new Comments_By_Post_Route();
-    $Custome_Route->register_routes();
+    $Comments_Route = new Comments_By_Post_Route();
+    $Nonce_Route = new Get_Nonce_By_Route();
+    $Comments_Route->register_routes();
+    $Nonce_Route->register_routes();
 });
